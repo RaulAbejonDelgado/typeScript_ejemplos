bf9e309437341ee4301775429090dd73e8c63edd
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-05-24 20:42:45 +0200

    const / let comment

--- let_const.js
+++ let_const.js
@@ -1,14 +1,23 @@
 "use strict";
-//archivo resultante del tsc let_const.ts
+//Variables let y Const
+//salida es adios
 var mensaje = "hola";
 if (true) {
     var mensaje = "adios";
 }
 console.log(mensaje);
-//Esta seria la traduccion con let observamos como se comporta con las Variables
-//dentro del alcance de una condicional
+//con let la salida es hola2
 var mensaje2 = "hola2";
 if (true) {
     var mensaje2_1 = "adios2";
 }
 console.log(mensaje2);
+//constantes han de ser inciializadas en el momento de su declaracion
+//en un futuro si queremos cambiar su valor en lineas posteriores a su declaracion
+//no sera posible
+//Observamos que con const dentro del if su comportamiento
+var OPCIONES = "todas";
+if (true) {
+    var OPCIONES_1 = "ninguna";
+}
+console.log(OPCIONES);
--- let_const.ts
+++ let_const.ts
@@ -17,3 +17,12 @@ if (true) {
 }
 
 console.log(mensaje2);
+
+//constantes han de ser inciializadas en el momento de su declaracion
+//en un futuro si queremos cambiar su valor en lineas posteriores a su declaracion
+//no sera posible
+const OPCIONES = "todas";
+if (true) {
+  const OPCIONES = "ninguna";
+}
+console.log(OPCIONES);
