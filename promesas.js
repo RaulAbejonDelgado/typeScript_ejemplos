fcc6f728535abfd7b0e5197982ba3f0629fd54e8
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-06-07 02:30:27 +0200

    promesas

---
+++ promesas.js
@@ -0,0 +1,21 @@
+"use strict";
+//del es6
+//hacer una funcion cuando una tarea sincrona termina una tarea
+//los argumentos de la promera son la funcion si funciono correctamente, la segunda la funcion si sale mal
+var prom1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log('Promesa terminada');
+        //termina bien
+        resolve();
+        //termina mal
+        reject();
+    }, 1500);
+});
+//cuando se ejecute la promera recive 2 parametros la primera hace referencia a
+//resolve y la segunda a reject
+//prom1.then(parametro_funcion_resolve,parametro_funcion_reject)
+prom1.then(function () {
+    console.log("Ejecucion correcta");
+}, function () {
+    console.error('Ejecutar por salir mal');
+});
