8b353da60c5af4ceb5f9de0b2cacb519c64a7f62
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-06-08 19:48:03 +0200

    definicion de clase basica en typescript

---
+++ clase_basica.js
@@ -0,0 +1,8 @@
+"use strict";
+var Partner = /** @class */ (function () {
+    function Partner() {
+    }
+    return Partner;
+}());
+var raul = new Partner();
+console.log(raul);
---
+++ clase_basica.ts
@@ -0,0 +1,13 @@
+class Partner{
+
+  name:string;
+  is_company:boolean;
+  city:string;
+  display_name:string;
+
+
+}
+
+let raul:Partner = new Partner();
+
+console.log(raul);
